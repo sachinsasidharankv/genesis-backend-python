b7803c426eeb11ed5fa820fe0c55e637feeb6083
anandu-kv <anandu.s@keyvalue.systems>
2024-09-27 16:43:02 +0530

    feat: added cors

--- src/api/main.py
+++ src/api/main.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 from fastapi import FastAPI, UploadFile, File, Form
+from starlette.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from src.chains import preprocess_pdf, qp_generation
@@ -9,15 +10,19 @@ from src.voice.websocket import websocket_endpoint
 
 
 class UserInput(BaseModel):
-    user_input: str
-
-
-class Feedback(BaseModel):
     context: dict
-    user_input: str
+    query: str
 
 
 app = FastAPI(title="FastAPI")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,  # Allows sending cookies and credentials
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allows all headers (e.g., Authorization, Content-Type, etc.)
+)
 
 UPLOAD_DIRECTORY = "uploads"
 Path(UPLOAD_DIRECTORY).mkdir(parents=True, exist_ok=True)
@@ -52,7 +57,7 @@ def generate_question_paper(req: UserInput):
 
 
 @app.get("/feedback")
-def feedback(req: Feedback):
+def feedback(req: UserInput):
     from src.agent import get_our_agent
     from langchain.memory import ConversationBufferMemory
 
@@ -64,7 +69,15 @@ def feedback(req: Feedback):
 
     chat_history = memory.buffer_as_messages
     response = agent_executor.invoke({
-            "input": f"Context: {req.context}\nStudent query: {req.user_input}",
-            "chat_history": chat_history,
-        })
+        "input": f"Context: {req.context}\nStudent query: {req.query}",
+        "chat_history": chat_history,
+    })
     print(response["output"])
+
+
+@app.post("/agent")
+def agent(req: UserInput):
+    return {
+        "context": req.context,
+        "query": req.query
+    }
